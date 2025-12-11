904a899aac86418d6d01942ff088908a01516e57
111111-jiang zhiyu <zhiyujiang364@gmail.com>
2025-12-11 23:32:50 +0800

    test: add CICD, but didn't tackle error, i don't know why product so many error,and i don't konw how to tackle them

--- service/video_service.go
+++ service/video_service.go
@@ -58,11 +58,11 @@ func (v *VideoService) GetPublishList(targetUserID uint, currentUserID uint) (*t
 	}, nil
 }
 
-func (v *VideoService) PublishVideo(userID uint, title, plyPath, coverPath string) error {
+func (v *VideoService) PublishVideo(userID uint, title, playPath, coverPath string) error {
 	video := &model.Video{
 		UserID:      userID,
 		Title:       title,
-		PlayURL:     plyPath,
+		PlayURL:     playPath,
 		CoverURL:    coverPath,
 		PublishTime: time.Now(),
 	}
